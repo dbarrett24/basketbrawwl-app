72b85061f358483171b4cf5f5a667b8706b1a45a
dbarrett24 <dbarrett24@gmail.com>
2017-09-13 13:39:13 -0600

    swapped slick to flickity carousel, need to fix routing to roster-view

--- public/dist/all.js
+++ public/dist/all.js
@@ -246,17 +246,30 @@ angular.module("brawlApp").controller("player-headerCtrl", function ($scope, mai
 angular.module("brawlApp").controller("team-choicesCtrl", function ($scope, mainService, $stateParams) {
     $(document).ready(function () {
         $('.player-header-1').css('position', 'relative');
-        $('.carousel-wrapper').slick({
-            infinite: true,
-            slidesToShow: 5,
-            slidesToScroll: 2,
-            autoplay: true,
-            autoplaySpeed: 3500
+        // $('.carousel-wrapper').slick({
+        //     infinite: true,
+        //     slidesToShow: 5,
+        //     slidesToScroll: 2,
+        //     autoplay: true,
+        //     autoplaySpeed: 3500
+        // });
+
+        $('.carousel-wrapper').flickity({
+            // options
+            cellSelector: '.carousel-cell',
+            cellAlign: 'left',
+            pageDots: false,
+            // groupCells: 3,
+            adaptiveHeight: false,
+            imagesLoaded: true,
+            autoPlay: true,
+            contain: true
         });
     });
 
     mainService.getTeams().then(function (teamData) {
         $scope.teamData = teamData;
+        console.log($scope.teamData);
     });
 });
 "use strict";
